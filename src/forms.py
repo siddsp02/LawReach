cd98864e17c931d67504e60263d0cde24226f323
Sidd <sidd.s.pai@gmail.com>
2024-11-09 15:16:57 -0700

    Added pages.

---
+++ src/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+
+class LawyerSignUpForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField("Sign Up")
+
+
+class ClientSignUpForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField("Sign Up")
--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
 # !usr/bin/env python3
 
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired
 
-
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    remember_me = BooleanField("Remember Me")
-    submit = SubmitField("Sign In")
-
+from forms import ClientSignUpForm, LawyerSignUpForm, LoginForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "key"  # This will have to be replaced.
@@ -21,50 +12,76 @@ app.config["SECRET_KEY"] = "key"  # This will have to be replaced.
 def index():
     return render_template("index.html", title="Home")
 
-# @app.route("/")
-# def index():
-#     return render_template('index.html', title='Home')
 
-@app.route("/login.html")
+@app.route("/login")
 def login():
     form = LoginForm()
     return render_template("login.html", form=form)
 
 
-@app.route("/signup.html")
+@app.route("/sign-up")
 def signup():
     return render_template("signup.html")
 
 
-@app.route("/client.html")
+@app.route("/client")
 def client():
     return render_template("client.html")
 
 
-@app.route("/laywer.html")
+@app.route("/lawyer")
 def lawyer():
     return render_template("lawyer.html")
 
 
-def main() -> str:  # type: ignore
-    index()
-    login()
-    signup()
-@app.route("/")
-def clientrequest():
-    data = [{
-        'id': 0,
-         'title': "I want to sue McDonalds",
-         'status': 0,
-         'client_id' : 1,
-         'lawyer_id': 1,
-         'subject': "foo",
-         'case_type': 'CLOSED',
-         'date_posted' : '08/08/2003',
-    }]
+@app.route("/lawyer-sign-up")
+def lawyer_sign_up():
+    form = LawyerSignUpForm()
+    return render_template("lawyer-sign-up.html", form=form)
 
-    return render_template('client_request.html', requests=data)
 
+@app.route("/client-sign-up")
+def client_sign_up():
+    form = ClientSignUpForm()
+    return render_template("client-sign-up.html", form=form)
+
+
+@app.route("/client-requests")
+def client_request():
+    data = [
+        {
+            "id": 0,
+            "title": "I want to sue McDonalds",
+            "status": 0,
+            "client_id": 1,
+            "lawyer_id": 1,
+            "subject": "foo",
+            "case_type": "CLOSED",
+            "date_posted": "08/08/2003",
+        },
+        {
+            "id": 2,
+            "title": "I want to sue McDonalds",
+            "status": 0,
+            "client_id": 1,
+            "lawyer_id": 1,
+            "subject": "foo",
+            "case_type": "CLOSED",
+            "date_posted": "08/08/2003",
+        },
+        {
+            "id": 1,
+            "title": "I want to sue McDonalds",
+            "status": 0,
+            "client_id": 1,
+            "lawyer_id": 1,
+            "subject": "foo",
+            "case_type": "CLOSED",
+            "date_posted": "08/08/2003",
+        },
+    ]
+
+    return render_template("client-request.html", requests=data)
 
 
 if __name__ == "__main__":
