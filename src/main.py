3559eeeb005de386bfef6dbb28884fb3d577aa9b
jenithion <109642224+jenithion@users.noreply.github.com>
2024-11-09 11:50:57 -0700

    Moved templates inside of src

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 # !usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main() -> str:
-    return "<p> Hello </p>"
+    return render_template('index.html', title='Home')
 
 
 if __name__ == "__main__":
